febe62e0ae0fed65797ff51c3d898a0f91ddb763
Piyyaaa <apriyantojordy7@gmail.com>
2025-09-18 23:10:41 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
-import type { Metadata } from 'next'
-import { defaultMetadata } from '../app/lib/Metadata'
+import type { Metadata, Viewport } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  ...defaultMetadata,
-  viewport: { width: 'device-width', initialScale: 1 },
+  title: 'Homepage Next.js',
+  description: '...',
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  themeColor: '#ffffff',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="id">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
       <body>{children}</body>
     </html>
   )
