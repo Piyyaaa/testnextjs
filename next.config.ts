ed7501e652ed36146941b0da0580f71f7f93953b
Piyyaaa <apriyantojordy7@gmail.com>
2025-09-18 22:52:34 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,12 @@
 import type { NextConfig } from 'next'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  productionBrowserSourceMaps: false,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ]
-  },
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: isProd ? '/testnextjs' : '',
+  assetPrefix: isProd ? '/testnextjs/' : '',
 }
 
 export default nextConfig
