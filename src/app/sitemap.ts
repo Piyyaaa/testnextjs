04c49b514dc1e2062a5044c48d463e889afad291
Piyyaaa <apriyantojordy7@gmail.com>
2025-09-18 23:38:34 +0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,19 +1,10 @@
-import { MetadataRoute } from 'next'
-import { siteContent } from '../app/lib/Content'
+export const dynamic = 'force-static'
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap() {
   return [
     {
-      url: siteContent.site.baseUrl,
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },
-    {
-      url: `${siteContent.site.baseUrl}/amp`,
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
+      url: 'https://piyyaaa.github.io/testnextjs',
+      lastModified: new Date().toISOString(),
     },
   ]
 }
