2d438e0d42a3afca20a86f1a7e357ff167e975f9
Piyyaaa <apriyantojordy7@gmail.com>
2025-09-19 09:52:21 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,6 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="id">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
       <body>{children}</body>
     </html>
   )
